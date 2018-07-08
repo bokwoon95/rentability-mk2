103a506766940e4dd441c646fcaf870e75965127
bokwoon <bokwoon.c@gmail.com>
2018-07-08 13:12:27 +0800

    Final Commit

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -15,11 +15,15 @@ class App extends Component {
 
     this.state = {
       hideCompleted: false,
+      shouldShowForm: false,
+      searchBarText: '',
+      currentuzer: '',
     };
   }
 
   handleSubmit(event) {
     event.preventDefault();
+    console.log(event);
 
     // Find the text field via the React ref
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
@@ -35,6 +39,43 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
+  handleSubmitv2(event) {
+    event.preventDefault();
+    console.log(event);
+
+    const item = ReactDOM.findDOMNode(this.refs.itemInput).value.trim();
+    // const condition = ReactDOM.findDOMNode(this.refs.conditionInput).value.trim();
+    const price = ReactDOM.findDOMNode(this.refs.priceInput).value.trim();
+    const address = ReactDOM.findDOMNode(this.refs.addressInput).value.trim();
+
+    Tasks.insert({
+      item,
+      price,
+      address,
+      createdAt: new Date(), // current time
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username,  // username of logged in user
+      name: Meteor.user().name,
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.itemInput).value = '';
+    ReactDOM.findDOMNode(this.refs.priceInput).value = '';
+    ReactDOM.findDOMNode(this.refs.addressInput).value = '';
+  }
+
+  // onInputChange(evt) {
+  //   const fields = Object.assign({}, this.state.fields);
+  //   fields[evt.target.name] = evt.target.value;
+  //   this.setState({fields});
+  // };
+
+  toggleForm() {
+    this.setState({
+      shouldShowForm: !this.state.shouldShowForm,
+    });
+  }
+
   toggleHideCompleted() {
     this.setState({
       hideCompleted: !this.state.hideCompleted,
@@ -43,20 +84,41 @@ class App extends Component {
 
   renderTasks() {
     let filteredTasks = this.props.tasks;
-    if (this.state.hideCompleted) {
-      filteredTasks = filteredTasks.filter(task => !task.checked);
-    }
+    // if (this.state.hideCompleted) {
+    //   filteredTasks = filteredTasks.filter(task => !task.checked);
+    // }
     return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
+      <Task key={task._id} task={task} cu={this.props.currentUser} />
     ));
+  }
+
+  onInputChange(evt) {
+    event.preventDefault();
+    console.log(evt.target.value);
+    this.setState({
+     searchBarText : evt.target.value,
+    });
+  }
+
+  onNameEnter(evt) {
+    event.preventDefault();
+    let namajeff = ReactDOM.findDOMNode(this.refs.ownerName).value.trim();
+    console.log(namajeff);
+    console.log(Meteor.userId());
+    Meteor.users.update(Meteor.userId(), {
+      $set: {
+        name: namajeff
+      }
+    });
   }
 
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Todo List ({this.props.incompleteCount})</h1>
+          {/*<h1>Todo List ({this.props.incompleteCount})</h1>*/}
 
+          {/*
           <label className="hide-completed">
             <input
               type="checkbox"
@@ -66,18 +128,88 @@ class App extends Component {
             />
             Hide Completed Tasks
           </label>
+              */}
 
           <AccountsUIWrapper />
 
+          {/*
           { this.props.currentUser ?
-            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-              <input
-                type="text"
-                ref="textInput"
-                placeholder="Type to add new tasks"
-              />
-            </form> : ''
+          <form className="new-task" onSubmit={this.onNameEnter.bind(this)}>
+            <p>jeff:{this.currentuzer}</p>
+            <input
+              type="text"
+              ref="ownerName"
+              placeholder={this.currentuzer ? "kimi no namae wa" : "who are you?"}
+            />
+            <button>nama jeff</button>
+          </form> : ''
           }
+          <br />
+          */}
+
+          { this.props.currentUser ?
+              this.state.shouldShowForm ?
+              <div>
+                <button onClick={this.toggleForm.bind(this)}>
+                  Close Entry
+                </button>
+                {/*
+                <form>
+                  <input
+                    type="text"
+                    ref="searchBar"
+                    placeholder="Search"
+                    onChange={this.onInputChange.bind(this)}
+                  />
+                </form>
+                */}
+                <form onSubmit={this.handleSubmitv2.bind(this)}>
+                  <input
+                  type="text"
+                  ref="itemInput"
+                  placeholder="Item name"
+                  />
+
+                {/*
+                  <input
+                  type="text"
+                  ref="conditionInput"
+                  placeholder="Condition of item"
+                  />
+                  */}
+
+                  <input
+                  type="text"
+                  ref="priceInput"
+                  placeholder="Price"
+                  />
+
+                  <input
+                  type="text"
+                  ref="addressInput"
+                  placeholder="Pickup Address"
+                  />
+                  <button>Submit</button>
+                </form>
+              </div>
+               :
+              <div>
+              <button onClick={this.toggleForm.bind(this)}>
+                New Entry
+              </button>
+              </div>
+              :
+            ''
+          }
+          {/*
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
+          */}
         </header>
 
         <ul>
@@ -92,6 +224,7 @@ export default withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+    // refinedSearch: Tasks.find({ "item": new RegExp(searchBarText) }).fetch(),
     currentUser: Meteor.user(),
   };
 })(App);
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -4,6 +4,14 @@ import { Tasks } from '../api/tasks.js';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showDetails: false,
+    };
+  }
+
   toggleChecked() {
     // Set the checked property to the opposite of its current value
     Tasks.update(this.props.task._id, {
@@ -15,6 +23,18 @@ export default class Task extends Component {
     Tasks.remove(this.props.task._id);
   }
 
+  showThisTask() {
+    this.setState({
+      showDetails: true
+    });
+  }
+
+  hideThisTask() {
+    this.setState({
+      showDetails: false
+    });
+  }
+
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
@@ -22,20 +42,41 @@ export default class Task extends Component {
 
     return (
       <li className={taskClassName}>
+        { this.props.cu ?
         <button className="delete" onClick={this.deleteThisTask.bind(this)}>
-          &times;
-        </button>
+          delete
+        </button> : ''
+        }
 
+        {this.state.showDetails ?
+        <button className="delete" onClick={this.hideThisTask.bind(this)}>
+          hide
+        </button>
+          :
+        <button className="delete" onClick={this.showThisTask.bind(this)}>
+          show
+        </button>
+        }
+        {/*
         <input
           type="checkbox"
           readOnly
           checked={!!this.props.task.checked}
           onClick={this.toggleChecked.bind(this)}
         />
+        */}
 
-        <span className="text">
-          <strong>{this.props.task.username}</strong>: {this.props.task.text}
-        </span>
+        {!this.state.showDetails ?
+          <span className="text">
+            <strong>{this.props.task.username}</strong>: {this.props.task.item}
+          </span>
+            :
+          <span className="text">
+            <strong>{this.props.task.username}</strong>: {this.props.task.item}
+            <p>Price: {this.props.task.price}</p>
+            <p>Address: {this.props.task.address}</p>
+          </span>
+        }
       </li>
     );
   }
